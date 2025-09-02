0c0ccd2cb1877a70af8fe8deff8ac2a2a191cf43
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2025-09-02 12:18:29 +0400

    update home

--- app/components/HomeClient.jsx
+++ app/components/HomeClient.jsx
@@ -312,40 +312,45 @@ export default function HomeClient() {
         </div>
       </div>
 
-      {/* As Seen */}
-      <div className="as-seen">
-        <div className="container">
-          <div className="as-seen-wrapper">
-            <h1>As Seen On</h1>
-            <div className="seen-wrap">
-              {seenIcons.map((x, i) => (
-                <div className="img-wrap" key={i}>
-                  <a href={seenLinks[i]} target="_blank" rel="noopener noreferrer">
-                    <img src={x} alt="seen icon" />
-                  </a>
-                </div>
-              ))}
+      <div className="logosection">
+            <div className="left">
+                  {/* As Seen */}
+                  <div className="as-seen">
+                    <div className="container">
+                      <div className="as-seen-wrapper">
+                        <h1>As Seen On</h1>
+                        <div className="seen-wrap">
+                          {seenIcons.map((x, i) => (
+                            <div className="img-wrap" key={i}>
+                              <a href={seenLinks[i]} target="_blank" rel="noopener noreferrer">
+                                <img src={x} alt="seen icon" />
+                              </a>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      {/* partners */}
-      <div className="as-seen">
-        <div className="container">
-          <div className="as-seen-wrapper">
-            <h1>Our Partners</h1>
-            <div className="seen-wrap">
-              {partnerIcons.map((x, i) => (
-                <div className="img-wrap" key={i}>
-                  <a href={partnerLinks[i]} target="_blank" rel="noopener noreferrer">
-                    <img src={x} alt="seen icon" />
-                  </a>
-                </div>
-              ))}
+            <div className="right">
+                  {/* partners */}
+                  <div className="as-seen">
+                    <div className="container">
+                      <div className="as-seen-wrapper">
+                        <h1>Our Partners</h1>
+                        <div className="seen-wrap">
+                          {partnerIcons.map((x, i) => (
+                            <div className="img-wrap" key={i}>
+                              <a href={partnerLinks[i]} target="_blank" rel="noopener noreferrer">
+                                <img src={x} alt="seen icon" />
+                              </a>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
             </div>
-          </div>
-        </div>
       </div>
     </>
   );
