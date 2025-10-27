99eadc829f741c4b61f424c0315aedfdf11da204
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2025-10-27 14:23:09 +0400

    update page

--- app/mocks/menuItems.js
+++ app/mocks/menuItems.js
@@ -87,6 +87,7 @@ export const menuItems = [
               submenuTitle: "Hydration",
               submenu: [
                 { label: "Signature Hydrafacial", to: "/aesthetic/hydra-facial" },
+                { label: "Oxygeneo Hydrafacial", to: "/aesthetic/oxygeneo-hydrafacial" },
                 { label: "Skin Boosters", to: "/aesthetic/skin-boosters" },
                 { label: "Collagen Threads", to: "/aesthetic/hydra-facial" },
                 { label: "Fillers", to: "/aesthetic/dermal-fillers" },
