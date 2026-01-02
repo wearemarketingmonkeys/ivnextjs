2af4b378b14ea28ced6f5b7d522cedd02b318561
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2026-01-02 17:17:52 +0400

    update footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -327,8 +327,8 @@ const Footer = () => {
                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
                     <polyline points="15 3 21 3 21 9" />
                     <line x1="10" y1="14" x2="21" y2="3" />
-                  </svg></Link>
-                <Link href="https://share.google/HWI0lHKteP0dIP5mF" className="btn btn-light">PALM
+                  </svg></Link> | 
+                <Link href="https://share.google/HWI0lHKteP0dIP5mF" className="btn btn-light">PALM JUMEIRAH
                 <svg
                   className="external-icon"
                   xmlns="http://www.w3.org/2000/svg"
