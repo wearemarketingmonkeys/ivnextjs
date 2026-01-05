7a2541c497c51fc3d3455e9533b5e4f09a3f78b4
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2026-01-05 13:07:17 +0400

    update menu

--- app/mocks/menuItems.js
+++ app/mocks/menuItems.js
@@ -106,7 +106,7 @@ export const menuItems = [
               submenuTitle: "The IV Wellness",
               submenu: [
                 { label: "About us", to: "/about-us" },
-                { label: "Meet Our Doctor", to: "/meet-our-doctor" },
+                // { label: "Meet Our Doctor", to: "/meet-our-doctor" },
                 { label: "Packages", to: "/packages" },
                 { label: "Blogs", to: "/blogs" },
                 { label: "Contact us", to: "/contact-us" },
