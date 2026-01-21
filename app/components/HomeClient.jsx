fff559cf84b21adb594df63ceabf432fc610e29e
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2026-01-21 17:30:55 +0400

    update home

--- app/components/HomeClient.jsx
+++ app/components/HomeClient.jsx
@@ -31,7 +31,7 @@ function FadeImage({ images = [], interval = 3000, className = "", altPrefix = "
       const t = setTimeout(() => {
         setIndex((prev) => (prev + 1) % images.length);
         setVisible(true);
-      }, 350); // must match CSS transition time
+      }, 450); // must match CSS transition time
 
       return () => clearTimeout(t);
     }, interval);
