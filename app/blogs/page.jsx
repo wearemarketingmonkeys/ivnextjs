cffe7a06f64bebb5241051e62e79cade39900928
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2026-02-09 17:34:37 +0400

    update blogs

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -41,7 +41,7 @@ export default async function BlogsPage({ searchParams }) {
   const articlesPerPage = 40;
 
   const res = await fetch("https://iv-blogs.ivhub.com/blogslist", {
-    next: { revalidate: 6 },
+    next: { revalidate: 600 },
   });
 
   if (!res.ok) {
