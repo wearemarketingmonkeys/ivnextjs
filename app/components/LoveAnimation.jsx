f5d8ef6dbd489e0e7ac4742979ea3f7ea1b3862c
wearemarketingmonkeys <70533684+wearemarketingmonkeys@users.noreply.github.com>
2026-02-12 17:11:11 +0400

    update animation

--- app/components/LoveAnimation.jsx
+++ app/components/LoveAnimation.jsx
@@ -18,13 +18,13 @@ export default function ValentinesConfetti() {
     const heart = document.createElement('div');
     heart.classList.add('heart');
     heart.style.left = Math.random() * 100 + 'vw';
-    heart.style.animationDuration = Math.random() * 10 + 15 + 's';
+    heart.style.animationDuration = Math.random() * 8 + 8 + 's';
     document.getElementById('confetti-container').appendChild(heart);
     // setTimeout(() => heart.remove(), 5000);
   };
 
   const triggerConfetti = () => {
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 5; i++) {
       setTimeout(createHeart, i * 1000);
     }
   };
